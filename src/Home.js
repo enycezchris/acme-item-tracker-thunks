dfa2ef5435250b9bba3bb080d77a78a8894e8858
enycezchris <enycezchris@gmail.com>
2022-07-13 17:58:18 -0400

    finish

--- src/Home.js
+++ src/Home.js
@@ -1,36 +1,40 @@
-import React from 'react';
-import { connect } from 'react-redux';
+import React from "react";
+import { connect } from "react-redux";
 
-const Home = ({ users, things, topRanked })=> {
+const Home = ({ users, things, topRanked, topRankedUser }) => {
   return (
     <div>
       <h1>Home</h1>
       <p>
-        Here at the Acme Item Tracker Corp we have { users.length } users and { things.length } things!
+        Here at the Acme Item Tracker Corp we have {users.length} users and{" "}
+        {things.length} things!
       </p>
       <h2>Top Ranked</h2>
       <ul>
-        {
-          topRanked.map( thing => {
-            return (
-              <li key={ thing.id }>
-                { thing.name }
-              </li>
-            );
-          })
-        }
+        {topRanked.map((thing) => {
+          return <li key={thing.id}>{thing.name}</li>;
+        })}
+      </ul>
+      <h2>Top User</h2>
+      <ul>
+        {topRankedUser.map((user) => {
+          return <li key={user.id}> {user.name} </li>;
+        })}
       </ul>
     </div>
   );
 };
 
-const mapSToP = (s)=> {
-  const topRank = Math.max(...s.things.map(thing => thing.ranking));
-  const topRanked = s.things.filter(thing => thing.ranking === topRank);
+const mapSToP = (s) => {
+  const topRank = Math.max(...s.things.map((thing) => thing.ranking));
+  const topRanked = s.things.filter((thing) => thing.ranking === topRank);
+  const topUser = Math.max(...s.users.map((user) => user.ranking));
+  const topRankedUser = s.users.filter((user) => user.ranking === topUser);
   return {
     users: s.users,
     things: s.things,
-    topRanked
+    topRanked,
+    topRankedUser,
   };
 };
 
--- src/Users.js
+++ src/Users.js
@@ -1,67 +1,87 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import axios from 'axios';
+import React from "react";
+import { connect } from "react-redux";
+import axios from "axios";
+import {
+  createUser,
+  removeThingFromUser,
+  deleteUser,
+  updateUserRank,
+} from "./store";
 
-
-const Users = ({ users, createUser, deleteUser, things, removeThingFromUser })=> {
+const Users = ({
+  users,
+  createUser,
+  deleteUser,
+  things,
+  removeThingFromUser,
+  updateUserRank,
+  increment
+}) => {
   return (
     <div>
       <h1>Users</h1>
-      <button onClick={ createUser }>+</button>
+      <button onClick={createUser}>+</button>
       <ul>
-        {
-          users.map( user => {
-            return (
-              <li key={ user.id }>
-                { user.name }
-                <button onClick={ ()=> deleteUser(user)}>x</button>
-                <ul>
-                {
-                  things.filter( thing => thing.userId === user.id)
-                    .map(thing => {
-                      return (
-                        <li key={ thing.id }>
-                          { thing.name } ({ thing.ranking })
-                          <button onClick={ ()=> removeThingFromUser(thing)}>x</button>
-                        </li>
-                      );
-                    }) 
-                  
-                }
-                </ul>
-              </li>
-            );
-          })
-        }
+        {users.map((user) => {
+          return (
+            <li key={user.id}>
+              {user.name} <br />
+              (Rating:{user.ranking})
+              <div>
+                <button onClick={() => deleteUser(user)}>x</button>
+                <button onClick={() => increment(user, -1)}>-</button>
+                <button onClick={() => increment(user, 1)}>+</button>
+              </div>
+              <ul>
+                {things
+                  .filter((thing) => thing.userId === user.id)
+                  .map((thing) => {
+                    return (
+                      <li key={thing.id}>
+                        {thing.name} ({thing.ranking})
+                        <button onClick={() => removeThingFromUser(thing)}>
+                          x
+                        </button>
+                      </li>
+                    );
+                  })}
+              </ul>
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
-}
+};
 
-const mapStateToProps = (state)=> {
+const mapStateToProps = (state) => {
   return {
     users: state.users,
-    things: state.things
+    things: state.things,
   };
-}
+};
 
-const mapDispatch = (dispatch)=> {
+const mapDispatch = (dispatch) => {
   return {
-    createUser: async()=> {
-      const user = (await axios.post('/api/users', {name: Math.random()})).data;
-      dispatch({ type: 'CREATE_USER', user});
-      //hint
-      //dispatch(createUser({name: Math.random()}));
+    createUser: () => {
+      dispatch(createUser({ name: Math.random() }));
     },
-    removeThingFromUser: async(thing)=> {
-      thing = {...thing, userId: null}
-      const updatedThing = (await axios.put(`/api/things/${thing.id}`, thing)).data
-      dispatch({ type: 'UPDATE_THING', thing: updatedThing});
+    removeThingFromUser: async (thing) => {
+      thing = { ...thing, userId: null };
+      console.log("thing", thing);
+      dispatch(removeThingFromUser(thing));
     },
-    deleteUser: async(user)=> {
-      await axios.delete(`/api/users/${user.id}`);
-      dispatch({ type: 'DELETE_USER', user});
+    deleteUser: async (user) => {
+      dispatch(deleteUser(user));
+    },
+    updateUserRank: async (user, userId, dir) => {
+      user = { ...user, userId: userId * 1 };
+      dispatch(updateUserRank(user));
+    },
+    increment: async (user, dir) => {
+      user = { ...user, ranking: user.ranking + dir };
+      dispatch(updateUserRank(user));
     },
   };
-}
+};
 export default connect(mapStateToProps, mapDispatch)(Users);
